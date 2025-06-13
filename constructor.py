6cc2d3586847d6c9e47d93c8c84ef6f9e6f095d0
Hetu <sahu.hetram@gmail.com>
2025-06-13 09:28:33 +0530

    OOP

--- constructor.py
+++ constructor.py
@@ -1,14 +1,22 @@
+# Doubt Session
+
 class example:
-    def __init__(self,name):
-        self.name = name
+
 
     def __init__(self, name,rollnum):
         self.name = name
         self.rollnum = rollnum
 
+    def __init__(self,name):
+        self.name = name
+
     def Printdetails(self):
         print(self.name ,self.rollnum)
 
 
-obj1 = example("ETL",10)
+# doesn't work , it will always consider the last constructor
+obj1 = example("ETL")
 obj1.Printdetails()
+
+obj2 = example("ETL",10)
+obj2.Printdetails()
